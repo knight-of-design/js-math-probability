892eff0f59228b17e9a4002a8ded90660fb5bf42
James Knight <knight.of.design@gmail.com>
2017-01-28 22:58:55 -0500

    Update marbles-2blue1red.js

--- marbles-2blue1red.js
+++ marbles-2blue1red.js
@@ -4,6 +4,14 @@
 // Y has 9 marbles - 4 blue 5 red
 // Z has 8 marbles - 4 blue 4 red
 // If 1 marble is taken from each bag, what is the probability of ending up with 1 blue and 2 red
+function gcd(x, y) {
+    while (y != 0) {
+        var z = x % y;
+        x = y;
+        y = z;
+    }
+    return x;
+}
 
 var SCALE = 7*9*12;
 var S2 = SCALE**2;
@@ -15,16 +23,6 @@ var YR = SCALE - YB
 var ZR = SCALE - ZB
 
 var prob = XR*(YR*ZB+ZR*YB) + XB*YR*ZR
-
-function gcd(x, y) {
-    while (y != 0) {
-        var z = x % y;
-        x = y;
-        y = z;
-    }
-    return x;
-}
-
 prob = prob/S2
 d = gcd(prob,SCALE)
 
